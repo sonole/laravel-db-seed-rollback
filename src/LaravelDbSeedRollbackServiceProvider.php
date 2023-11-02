49d04a16bbe34e47a52a116dd8cdf80ea136fe23
Alexandros Paliampelos <f5890620@gmail.com>
2023-11-02 15:01:37 +0200

    return a diff db:seed class

--- src/LaravelDbSeedRollbackServiceProvider.php
+++ src/LaravelDbSeedRollbackServiceProvider.php
@@ -6,5 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelDbSeedRollbackServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->app->singleton('command.seed', function ($app) {
+            $resolver = $app->make(\Illuminate\Database\ConnectionResolverInterface::class);
+            return new \Sonole\LaravelDbSeedRollback\Illuminate\Database\Console\Seeds\SeedCommand($resolver);
+        });
+        $this->commands('command.seed');
+    }
 
 }
